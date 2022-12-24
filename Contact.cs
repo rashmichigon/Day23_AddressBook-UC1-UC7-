350ff62c19b27dd33a85986798a297b3156fa555
Rashmi <rashmichigon65@gmail.com>
2022-12-24 17:06:55 +0530

    UC2-Add

--- Contact.cs
+++ Contact.cs
@@ -8,5 +8,27 @@ namespace AddressBook_UC5_UC10_
 {
     internal class Contact
     {
+        List<Person> person1 = new List<Person>();
+        public void addcontact()
+        {
+            Person person = new Person();
+            Console.Write("Enter First Name: ");
+            person.FirstName = Console.ReadLine();
+            Console.Write("Enter Last Name: ");
+            person.LastName = Console.ReadLine();
+            Console.Write("Enter Phone Number: ");
+            person.PhoneNo = Console.ReadLine();
+            Console.Write("Enter Address : ");
+            Console.Write("Enter Address : ");
+            person.Address = Console.ReadLine();
+            Console.Write("Enter city : ");
+            person.City = Console.ReadLine();
+            Console.Write("Enter pinCode : ");
+            person.ZipCode = Console.ReadLine();
+            Console.Write("Enter EmailId : ");
+            person.EmailId = Console.ReadLine();
+            person1.Add(person);
+            Console.ReadLine();
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -4,22 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Enter your First Name");
-            string firstName = Console.ReadLine();
-            Console.WriteLine("Enter your Last Name");
-            string lastName = Console.ReadLine();
-            Console.WriteLine("Enter your Address");
-            string address = Console.ReadLine();
-            Console.WriteLine("Enter your State");
-            string state = Console.ReadLine();
-            Console.WriteLine("Enter your ZipCode");
-            string zipcode = Console.ReadLine();
-            Console.WriteLine("Enter your Email_Id");
-            string emailId = Console.ReadLine();
-            Console.WriteLine("Enter your Phone_Number");
-            string phoneNo = Console.ReadLine();
-            Console.WriteLine($"\n First Name : {firstName}\n Last Name : {lastName}\n Address : {address}\n State : {state}\n ZipCode : {zipcode} \n Email ID : {emailId} \n Phone Number : {phoneNo}");
+            Contact person = new Contact();
+            person.addcontact();
         }
     }
 }
